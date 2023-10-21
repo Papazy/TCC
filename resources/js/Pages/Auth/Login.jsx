a0d9f2cca6a7e5cc9f6a0636ed35ec893da8b355
alfnsnff <alfnsnff>
2023-10-21 16:22:17 +0700

    Create login register interface

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -1,10 +1,13 @@
 import { useEffect } from 'react';
 import Checkbox from '@/Components/Checkbox';
+import CheckboxCustom from '@/Components/CheckboxCustom';
 import GuestLayout from '@/Layouts/GuestLayout';
 import InputError from '@/Components/InputError';
 import InputLabel from '@/Components/InputLabel';
 import PrimaryButton from '@/Components/PrimaryButton';
+import PrimaryButtonCustom from '@/Components/PrimaryButtonCustom';
 import TextInput from '@/Components/TextInput';
+import TextInputCustom from '@/Components/TextInputCustom';
 import { Head, Link, useForm } from '@inertiajs/react';
 
 export default function Login({ status, canResetPassword }) {
@@ -33,15 +36,22 @@ export default function Login({ status, canResetPassword }) {
             {status && <div className="mb-4 font-medium text-sm text-green-600">{status}</div>}
 
             <form onSubmit={submit}>
+
+                <div>
+                    <p className='text-4xl font-bold w-full my-4'>Log In</p>
+                </div>
+
                 <div>
                     <InputLabel htmlFor="email" value="Email" />
 
-                    <TextInput
+                    <TextInputCustom
+                    // Unset
                         id="email"
                         type="email"
                         name="email"
                         value={data.email}
                         className="mt-1 block w-full"
+                        // Default
                         autoComplete="username"
                         isFocused={true}
                         onChange={(e) => setData('email', e.target.value)}
@@ -53,7 +63,7 @@ export default function Login({ status, canResetPassword }) {
                 <div className="mt-4">
                     <InputLabel htmlFor="password" value="Password" />
 
-                    <TextInput
+                    <TextInputCustom
                         id="password"
                         type="password"
                         name="password"
@@ -66,30 +76,39 @@ export default function Login({ status, canResetPassword }) {
                     <InputError message={errors.password} className="mt-2" />
                 </div>
 
-                <div className="block mt-4">
+                <div className="flex items-center justify-between mt-4">
                     <label className="flex items-center">
-                        <Checkbox
+                        <CheckboxCustom
                             name="remember"
                             checked={data.remember}
                             onChange={(e) => setData('remember', e.target.checked)}
                         />
                         <span className="ml-2 text-sm text-gray-600">Remember me</span>
                     </label>
-                </div>
-
-                <div className="flex items-center justify-end mt-4">
+                    
                     {canResetPassword && (
                         <Link
                             href={route('password.request')}
-                            className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                            className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-none "
                         >
                             Forgot your password?
                         </Link>
                     )}
+                </div>
 
-                    <PrimaryButton className="ml-4" disabled={processing}>
+                <div className="block mt-4">
+                    <PrimaryButtonCustom className="" disabled={processing}>
                         Log in
-                    </PrimaryButton>
+                    </PrimaryButtonCustom>
+                </div>
+
+                <div className="flex items-center mt-4">
+                    <Link
+                        href={route('register')}
+                        className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-none focus:ring-offset-2 focus:ring-indigo-500"
+                    >
+                        Not have account?
+                    </Link>
                 </div>
             </form>
         </GuestLayout>
