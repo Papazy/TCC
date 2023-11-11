32b2e4929ec334881ad07e72cd7a243a03d990ab
alfnsnff <alfnsnff>
2023-11-11 08:34:41 +0700

    fix some changes

--- resources/js/Pages/ProblemDetail.jsx
+++ resources/js/Pages/ProblemDetail.jsx
@@ -6,6 +6,12 @@ import React, { useState } from "react";
 
 const ProblemDetail = ({ auth }) => {
 
+    // const { solve } = useForm({
+    //     email: '',
+    //     password: '',
+    //     remember: false,
+    // });
+
     const [data] = useState(
         [
             { id: 1, name: "Sepuh", time: 20 },
@@ -18,6 +24,18 @@ const ProblemDetail = ({ auth }) => {
             { id: 8, name: "Andi", time: 10 },
             { id: 9, name: "Ridwan", time: 10 },
             { id: 10, name: "Putri", time: 10 },
+        ]
+    );
+    
+    const [limits] = useState(
+        [
+            { memory: '128mb ', time: '10ms' }
+        ]
+    );
+
+    const [language] = useState(
+        [
+            { value: "C++" }
         ]
     );
 
@@ -33,37 +51,41 @@ const ProblemDetail = ({ auth }) => {
                 </div>
                 
 
-                
-                <table class="border-separate border border-slate-500 ...">
-                <thead>
-                    <tr>
-                    <th class="px-10 border border-slate-600 ...">Time Limit</th>
-                    <th class="px-10 border border-slate-600 ...">Memory Limit</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                    <td class="text-center border border-slate-700 ...">Indiana</td>
-                    <td class="text-center border border-slate-700 ...">Indianapolis</td>
-                    </tr>
-                </tbody>
-                </table>
+                {limits.map((limit) => (
+                    <table class="border-separate border border-slate-500 ...">
+                        <thead>
+                            <tr>
+                            <th class="px-10 border border-slate-600 ...">Time Limit</th>
+                            <th class="px-10 border border-slate-600 ...">Memory Limit</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                            <td class="text-center border border-slate-700 ...">{limit.time}</td>
+                            <td class="text-center border border-slate-700 ...">{limit.memory}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                ))}
 
                 <object className='my-10 border-2 border-black w-full' data="https://media.geeksforgeeks.org/wp-content/cdn-uploads/20210101201653/PDF.pdf"
-                    height="720"
-                ></object>
+                height="720"
+                >
+                </object>
 
                 <div>
                     <p className='text-xl font-bold w-full my-4'>Language</p>
                 </div>
-
-                <CustomLabel value="C++"/>
+                
+                {language.map((lang) => (
+                    <CustomLabel value={lang.value} className='w-24'/>
+                ))}
 
                 <div>
                     <p className='text-xl font-bold w-full my-4'>Submmit Solution</p>
                 </div>
 
-                <form class="flex space-x-2 border">
+                <form class="flex space-x-2 ">
                     <label class="w-full border border-b-2 border-black">
                         <input type="file" class="block w-full text-sm text-slate-800
                         file:py-3 file:px-4
@@ -73,7 +95,7 @@ const ProblemDetail = ({ auth }) => {
                         hover:file:bg-gray-700
                         "/>
                     </label>
-                    <PrimaryButtonCustom className='w-1/6'>
+                    <PrimaryButtonCustom className='w-40'>
                         Submit
                     </PrimaryButtonCustom>
                 </form>
