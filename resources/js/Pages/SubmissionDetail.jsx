84243daaabd701f05ebeb64cc15980536494ac43
alfnsnff <alfnsnff>
2023-11-11 16:33:22 +0700

    add tabbing

--- resources/js/Pages/SubmissionDetail.jsx
+++ resources/js/Pages/SubmissionDetail.jsx
@@ -6,8 +6,13 @@ import { Link, Head } from "@inertiajs/react";
 
 export default function SubmissionDetail({ auth }) {
 
-    const [data, setData] = useState(
-        [
+    const [currentTab, setCurrentTab] = useState(1);
+
+    const toogleTab = (index) => {
+        setCurrentTab(index);
+    }
+
+    const data = [
             { id: 1011010, user: "Sepuh", archive: "Tester", problems: "Tugas Menumpuk", lang: "C++", verdict: "Act", pts: 100, time: "1 Min ago"},
             { id: 1011011, user: "Yelan", archive: "Tester", problems: "Tugas Menumpuk", lang: "C++", verdict: "Act", pts: 100, time: "1 Min ago"},
             { id: 1011012, user: "Furina", archive: "Tester", problems: "Tugas Menumpuk", lang: "C++", verdict: "Act", pts: 100, time: "1 Min ago"},
@@ -18,8 +23,7 @@ export default function SubmissionDetail({ auth }) {
             { id: 1011017, user: "Kazuha", archive: "Tester", problems: "Tugas Menumpuk", lang: "C++", verdict: "Act", pts: 100, time: "1 Min ago"},
             { id: 1011018, user: "Hu Tao", archive: "Tester", problems: "Tugas Menumpuk", lang: "C++", verdict: "Act", pts: 100, time: "1 Min ago"},
             { id: 1011019, user: "Fagih", archive: "Tester", problems: "Tugas Menumpuk", lang: "C++", verdict: "Act", pts: 100, time: "1 Min ago"}
-        ]
-    );
+    ];
 
     const desc = [
         { id: 1, name: 'Asep', lang: 'C++', time: 'Oct 9, 2023 at 22:23:13', status: 'Complete' },
@@ -36,71 +40,14 @@ export default function SubmissionDetail({ auth }) {
             dataLinks = { problemLinks }
         >
             <Head title="Submission" />
-            <div className="">
-
-                <div>
-                    <p className='text-4xl font-bold w-full my-4'>Problem name</p>
-                </div>
-
+            <div className='flex justify-center'>
                 <div className='flex justify-center'>
-                    <div className='flex justify-center border-2 border-black w-max px-2 py-1 gap-2 rounded-full'>
-                        <Link className='hover:text-[#FFC900]'>Submissions</Link>
-                        {/* <span className='border-l-2 border-black'></span> */}
-                        <Link className='hover:text-[#FFC900]'>My Submisions</Link>
-                    </div>
+                    <button className='border-2 border-black py-1 px-2 rounded-l-full hover:bg-[#FFC900]' onClick={() => toogleTab(1)}>My Submision</button>
+                    {/* <span className='border-l-2 border-black'></span> */}
+                    <button className='border-2 border-black border-l-0 py-1 px-2 rounded-r-full hover:bg-[#FFC900]' onClick={() => toogleTab(2)}>All Submissions</button>
                 </div>
-                {/* <RankingsLayout></RankingsLayout> */}
- 
-                
-                <div className='pt-5'>
-                    <div class="table-wrapper">
-                        <table class="w-full border-separate border-spacing-y-3">
-                            <thead>
-                                <tr className="text-left">
-                                    <th className="pl-3">Id</th>
-                                    <th>User</th>
-                                    <th>Archive</th>
-                                    <th>Problems</th>
-                                    <th>Lang</th>
-                                    <th>Verdict</th>
-                                    <th>Pts</th>
-                                    <th>Time</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {data.map((item, index) => (
-                                    <tr key={index} className={index%2 === 0? 'bg-white' : 'bg-[#EDEDED]'}>
-                                        <td className={`border-y-2 border-s-2 border-black h-10 pl-3`}>
-                                            {item.id}
-                                        </td>
-                                        <td className={`border-y-2 border-black`}>
-                                            {item.user}
-                                        </td>
-                                        <td className={`border-y-2 border-black`}>
-                                            {item.archive}
-                                        </td>
-                                        <td className={`border-y-2 border-black`}>
-                                            {item.problems}
-                                        </td>
-                                        <td className={`border-y-2 border-black`}>
-                                            {item.lang}
-                                        </td>
-                                        <td className={`border-y-2 border-black`}>
-                                            {item.verdict}
-                                        </td>
-                                        <td className={`border-y-2 border-black`}>
-                                            {item.pts}
-                                        </td>
-                                        <td className={`border-y-2 border-e-2 border-black`}>
-                                            {item.time}
-                                        </td>
-                                    </tr>
-                                ))}
-                            </tbody>
-                        </table>    
-                    </div>
-                </div>
-
+            </div>
+            <div className={`${currentTab === 1 ? "" : "hidden"}`}>
                 <div>
                     <div>
                         <p className='text-4xl font-bold w-full my-4'>Problem name</p>
@@ -165,6 +112,65 @@ export default function SubmissionDetail({ auth }) {
                     </div>
                 </div>
             </div>
+            <div className={`${currentTab === 2 ? "" : "hidden"}`}>
+
+                <div>
+                    <p className='text-4xl font-bold w-full my-4'>Problem name</p>
+                </div>
+
+                {/* <RankingsLayout></RankingsLayout> */}
+ 
+                
+                <div className='pt-5'>
+                    <div class="table-wrapper">
+                        <table class="w-full border-separate border-spacing-y-3">
+                            <thead>
+                                <tr className="text-left">
+                                    <th className="pl-3">Id</th>
+                                    <th>User</th>
+                                    <th>Archive</th>
+                                    <th>Problems</th>
+                                    <th>Lang</th>
+                                    <th>Verdict</th>
+                                    <th>Pts</th>
+                                    <th>Time</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {data.map((item, index) => (
+                                    <tr key={index} className={index%2 === 0? 'bg-white' : 'bg-[#EDEDED]'}>
+                                        <td className={`border-y-2 border-s-2 border-black h-10 pl-3`}>
+                                            {item.id}
+                                        </td>
+                                        <td className={`border-y-2 border-black`}>
+                                            {item.user}
+                                        </td>
+                                        <td className={`border-y-2 border-black`}>
+                                            {item.archive}
+                                        </td>
+                                        <td className={`border-y-2 border-black`}>
+                                            {item.problems}
+                                        </td>
+                                        <td className={`border-y-2 border-black`}>
+                                            {item.lang}
+                                        </td>
+                                        <td className={`border-y-2 border-black`}>
+                                            {item.verdict}
+                                        </td>
+                                        <td className={`border-y-2 border-black`}>
+                                            {item.pts}
+                                        </td>
+                                        <td className={`border-y-2 border-e-2 border-black`}>
+                                            {item.time}
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>    
+                    </div>
+                </div>
+            </div>
+
         </SubLayout>
   );
 };
