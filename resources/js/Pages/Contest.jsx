cb1a8318220a3a78e5fde5599da6244996e88f50
kiramDZ <hilmi0@mhs.unsyiah.ac.id>
2023-11-21 10:42:18 +0700

    Perbaiki Router

--- resources/js/Pages/Contest.jsx
+++ resources/js/Pages/Contest.jsx
@@ -1,16 +1,32 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "@/Components/Navbar";
 import ContestTop from "@/Components/Contest/ContestTop";
 import ContestCard from "@/Components/Contest/ContestCard";
 
-export default function Contest() {
+const Contest = () => {
     return (
-        <>
-            <Navbar />
-            <div className="overflow-hidden font-mono md:min-w-fit  py-2 md:px-8 px-6 xl:min-w-full">
-                <ContestTop />
-                <ContestCard />
-                
-            </div>
-        </>
+        <Router>
+            <>
+                <Navbar />
+                <div className="overflow-hidden font-mono md:min-w-fit  py-2 md:px-8 px-6 xl:min-w-full">
+                    <Routes>
+                        <Route path="/contest" element={<MainContest />} />
+                        {/* <Route path="/contest/detail/:slug"></Route> */}
+                    </Routes>
+                </div>
+            </>
+        </Router>
+    );
+};
+
+function MainContest() {
+    return (
+        <div>
+            <ContestTop />
+            <ContestCard />
+        </div>
     );
 }
+
+export default Contest;
